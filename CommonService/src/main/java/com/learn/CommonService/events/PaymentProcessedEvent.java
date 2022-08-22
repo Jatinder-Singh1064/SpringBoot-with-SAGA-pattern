54de7a52221cafa67e3c187daf62392fbd9e9acb
Jatinder Singh <jatinder.1064@gmail.com>
2022-08-22 16:52:32 -0400

    common service added
    common service added which is shared between multiple microservices to implement Order Service SAGA

---
+++ CommonService/src/main/java/com/learn/CommonService/events/PaymentProcessedEvent.java
@@ -0,0 +1,13 @@
+package com.learn.CommonService.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentProcessedEvent {
+    private String paymentId;
+    private String orderId;
+}
